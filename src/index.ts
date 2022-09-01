a6a0fd18fbfb9a822ed6404795e2a2d3155fa673
Marcelo Alves <xfer@users.noreply.github.com>
2022-08-31 21:53:42 -0300

    Removing credentials/keyfile requirement

--- src/index.ts
+++ src/index.ts
@@ -115,10 +115,6 @@ export default class MulterGoogleCloudStorage implements multer.StorageEngine {
 			throw new Error('You have to specify project id for Google Cloud Storage to work.');
 		}
 
-		if (!opts.keyFilename && !opts.credentials) {
-			throw new Error('You have to specify credentials key file or credentials object, for Google Cloud Storage to work.');
-		}
-
 		this.gcsStorage = new Storage({
 			projectId: opts.projectId,
 			keyFilename: opts.keyFilename,
